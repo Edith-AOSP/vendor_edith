f4c11767e4be62ff809a02f4734efe1d831ff03b
Waxaranai <waxaranai@gmail.com>
2026-08-25 02:53:40 +0000

    edith: version: release Amber Amaryllis

--- config/ota.mk
+++ config/ota.mk
@@ -1,9 +1,6 @@
 # Updater
-ifeq ($(IS_OFFICIAL),true)
 PRODUCT_PACKAGES += \
     Updater
 
 PRODUCT_COPY_FILES += \
     vendor/edith/config/permissions/privapp-permissions-edith.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-edith.xml
-
-endif
--- config/version.mk
+++ config/version.mk
@@ -3,6 +3,7 @@ EDITH_HOST_TIME := $(shell date +"%Y%m%d")
 EDITH_BUILD_TYPE ?= Community
 
 EDITH_VERSION_NUMBER := 17.0.0
+EDITH_VERSION_CODENAME := Amber_Amaryllis
 
 EDITH_BUILD_VERSION := edith_$(EDITH_BUILD)-$(EDITH_VERSION_NUMBER)-$(EDITH_HOST_TIME)-$(EDITH_BUILD_TYPE)
 EDITH_BUILD_NUMBER := $(EDITH_VERSION_NUMBER).$(EDITH_HOST_TIME)
@@ -11,6 +12,7 @@ EDITH_BUILD_NUMBER := $(EDITH_VERSION_NUMBER).$(EDITH_HOST_TIME)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
    ro.edith.device=$(EDITH_BUILD) \
    ro.edith.version=$(EDITH_VERSION_NUMBER) \
+   ro.edith.version_codename=$(EDITH_VERSION_CODENAME) \
    ro.edith.build.version=$(EDITH_BUILD_VERSION) \
    ro.edith.build.number=$(EDITH_BUILD_NUMBER) \
    ro.edith.build.type=$(EDITH_BUILD_TYPE)
